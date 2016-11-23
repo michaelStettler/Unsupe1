b2c4cd5b26b8713241ba7a806d1142e7c0195d08
djeeb <djeeb.nekobus@gmail.com>
2016-11-23 13:19:13 +0100

    merge
    Merge

--- Unspe1.py
+++ Unspe1.py
@@ -9,33 +9,23 @@ from kohonen import *
 from Supplementary import *
 import numpy as np
 
-
 name = "Stettler"
 nameDigit = name2digits(name)
 print(nameDigit)
 
-
 # ### import data
-
 data = np.loadtxt("data.txt")
 labels = np.loadtxt("labels.txt")
-
-
 
 data_filtered, labels_flitered = filterData(data,labels,name)
 print(np.shape(data_filtered))
 #visualizeSample(data[0])
 
-
-centers = run_kohonen(data_filtered, size_k=6, sigma=2.0, eta=0.1, tmax=30000)
+centers = run_kohonen(data_filtered, size_k=6, sigma=3.0, eta=0.1, tmax=30000)
 visualizeSample(centers, size_k=6)
-
-
 
 average_digit = averageSample(data_filtered, labels_flitered, nameDigit)
 visualizeSample(average_digit)
-
-
 
 digits = []
 for center in centers:
